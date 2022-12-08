b5a2619316e1e4912e5141d0e66a0c92a5960e80
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-08 14:56:48 -0700

    Se quita team y puntos que estaban se agrega reviews y se quita fotos

--- modules/partners.php
+++ modules/partners.php
@@ -20,12 +20,12 @@
     <section class="wpo-about-section section-padding">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-6 col-12">
+                <!-- <div class="col-lg-6 col-12">
                     <div class="about-left">
                         <img src="assets/images/about2.jpg" alt="">
                     </div>
-                </div>
-                <div class="col-lg-6 col-12">
+                </div> -->
+                <div class="col-lg-12 col-12">
                     <div class="about-right">
                         <div class="text">
                             <span>Tu mejor opción en compra-venta de inmuebles</span>
@@ -42,240 +42,68 @@
     </section>
     <!--end wpo-about-section -->
 
-    <!-- start wpo-service-section -->
-    <section class="wpo-service-section">
-        <div class="container">
+<!-- start wpo-testimonial-section -->
+<div class="wpo-testimonial-section section-padding" style="margin-top: 1rem;">
+    <div class="container-fluid">
+        <div class="testimonial-wrapper text-center">
             <div class="row justify-content-center">
-                <div class="col-lg-6 col-12">
-                    <div class="wpo-section-title">
-                        <span>En Phoenix Legal services estamos orgullosos de formar parte de las mejores
-                            empresas</span>
-                        <h2 style="color:#AC8D68 ;">Phoenix Legal Services</h2>
-                        <!-- <div class="shape-title">
-                        <img src="assets/images/section-title-shape.png" alt="">
-                    </div> -->
+                <div class="col-lg-5 col-12">
+                    <div class="testimonial-slider">
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Tanneis Akrasenko</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Funky Dos</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>RockinIt</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>David Gray</h3>
+                            <span>Business Man</span>
+                        </div>
+                        <div class="testimonial-text">
+                            <img src="assets/images/ft-icon/5.png" alt="">
+                            <p>Lorem Ipsum is simply dummy
+                                the printing and typesetting industry
+                                has been the industry's standard text
+                                survived not only five centuries.</p>
+                            <h3>Tanneis Akrasenko</h3>
+                            <span>Business Man</span>
+                        </div>
                     </div>
                 </div>
+              
             </div>
-            <div class="scervice-wrap">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4 col-md-4 col-12">
-                        <div class="scervice-item">
-                            <div class="scervice-item-img">
-                                <img src="assets/images/services/icon-1.png" alt="">
-                            </div>
-                            <div class="scervice-item-text">
-                                <a href="#">
-                                    <h2>Compañías inmobiliarias</h2>
-                                    <span></span>
-                                </a>
-                                <!-- <p>It seem that only fragmen
-                                the original remain.</p> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-12">
-                        <div class="scervice-item">
-                            <div class="scervice-item-img">
-                                <img src="assets/images/services/icon-2.png" alt="">
-                            </div>
-                            <div class="scervice-item-text">
-                                <a href="#">
-                                    <h2>Promotores<br><br></h2>
-                                </a>
-                                <!-- <p>It seem that only fragmen
-                                the original remain.</p> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-12">
-                        <div class="scervice-item">
-                            <div class="scervice-item-img">
-                                <img src="assets/images/services/icon-3.png" alt="">
-                            </div>
-                            <div class="scervice-item-text">
-                                <a href="#">
-                                    <h2>Abogados de cierres <br><br></h2>
-                                </a>
-                                <!-- <p>It seem that only fragmen
-                                the original remain.</p> -->
-                            </div>
-                        </div>
-                    </div>
 
-                </div>
-            </div>
         </div>
-    </section>
-    <!-- end wpo-service-section -->
+    </div>
+</div>
+<!-- end wpo-testimonial-section -->
 
-
-    <!-- start wpo-team-section -->
-    <section class="wpo-team-section section-padding">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6 col-12">
-                    <div class="wpo-section-title">
-                        <h2>Conoce a nuestro equipo</h2>
-                        <span>Nos dedicamos a la experiencia de nuestros clientes, ofreciendo sólo las mejores
-                            soluciones para cada caso.</span>
-                        <!-- <div class="shape-title">
-                            <img src="assets/images/section-title-shape-2.png" alt="">
-                        </div> -->
-                    </div>
-                </div>
-            </div>
-            <div class="team-wrapper">
-                <div class="row wpo-team-slider">
-                    <div class="col-lg-3 col-12">
-                        <div class="team-single">
-                            <div class="team-single-img">
-                                <img src="assets/images/team/1.jpg" alt="">
-                            </div>
-                            <div class="team-single-text">
-                                <a href="team-single.html">
-                                    <h2>Henry Bannet</h2>
-                                </a>
-                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvasExample"
-                                    role="button" aria-controls="offcanvasExample" style="border:solid 1px;">
-                                    Ver más
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-12">
-                        <div class="team-single">
-                            <div class="team-single-img">
-                                <img src="assets/images/team/2.jpg" alt="">
-                            </div>
-                            <div class="team-single-text">
-                                <a href="team-single.html">
-                                    <h2>Tanean Akrasen</h2>
-                                </a>
-                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas2Example"
-                                    role="button" aria-controls="offcanvas2Example" style="border:solid 1px;">
-                                    Ver más
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col-lg-3 col-12">
-                        <div class="team-single">
-                            <div class="team-single-img">
-                                <img src="assets/images/team/3.jpg" alt="">
-                            </div>
-                            <div class="team-single-text">
-                                <a href="team-single.html">
-                                    <h2>Lily Margerat</h2>
-                                </a>
-                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas3Example"
-                                    role="button" aria-controls="offcanvas3Example" style="border:solid 1px;">
-                                    Ver más
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col-lg-3 col-12">
-                        <div class="team-single">
-                            <div class="team-single-img">
-                                <img src="assets/images/team/4.jpg" alt="">
-                            </div>
-                            <div class="team-single-text">
-                                <a href="team-single.html">
-                                    <h2>William Watson</h2>
-                                </a>
-                                <a class="btn theme-btn mt-1" data-bs-toggle="offcanvas" href="#offcanvas4Example"
-                                    role="button" aria-controls="offcanvas4Example" style="border:solid 1px;">
-                                    Ver más
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-    </section>
-
-    <!-- Menú lateral de los agentes -->
-    <section class="info-agentes">
-        <!-- Info 1 -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample"
-            aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 1</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body text-center">
-                <img src="assets/images/team/1.jpg" alt="">
-                <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
-                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
-                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
-                    aliquam, amet atque!
-                </p>
-            </div>
-        </div>
-        <!-- Info 2 -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas2Example"
-            aria-labelledby="offcanvas2ExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 2</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas2"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body text-center">
-                <img src="assets/images/team/1.jpg" alt="">
-                <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
-                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
-                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
-                    aliquam, amet atque!
-                </p>
-            </div>
-        </div>
-        <!-- Info 3 -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas3Example"
-            aria-labelledby="offcanvas3ExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 3</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas3"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body text-center">
-                <img src="assets/images/team/1.jpg" alt="">
-                <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
-                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
-                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
-                    aliquam, amet atque!
-                </p>
-            </div>
-        </div>
-        <!-- Info 4 -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas4Example"
-            aria-labelledby="offcanvas4ExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nombre Agente 4</h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas4"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body text-center">
-                <img src="assets/images/team/1.jpg" alt="">
-                <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sed aliquam molestias non asperiores
-                    cum, harum voluptatum aspernatur quidem perspiciatis aliquid recusandae, maxime accusamus nesciunt.
-                    Suscipit commodi assumenda tenetur cupiditate, ad in officiis dignissimos deleniti officia magnam
-                    aliquam, amet atque!
-                </p>
-            </div>
-        </div>
-    </section>
 
     <!-- start wpo-cta-section -->
     <section class="wpo-cta-section">
