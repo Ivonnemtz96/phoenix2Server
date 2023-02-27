ee3f30a6c9da972739325bff2add41c90b481424
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-02-27 13:56:48 -0700

    Se hacen las traducciones de dos archivos

---
+++ es/footer.php
@@ -0,0 +1,91 @@
+<footer class="wpo-site-footer">
+    <div class="wpo-upper-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col col-lg-4 col-md-6 col-sm-12 col-12">
+                    <div class="widget about-widget">
+                        <div class="logo widget-title">
+                            <img style="width: 15rem;" src="/assets/images/Logo-bco.png" alt="">
+                        </div>
+                        <p>
+                            Phoenix Legal Services, estamos orgullosos de asociarnos con los mejores del sector.
+                        </p>
+                        <!-- <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="ti-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="ti-instagram"></i>
+                                </a>
+                            </li>
+                        </ul> -->
+                    </div>
+                </div>
+                <div class="col  col-lg-4 col-md-6 col-sm-12 col-12">
+                    <div class="widget link-widget">
+                        <div class="widget-title">
+                            <h3>Nuestros servicios</h3>
+                        </div>
+                        <ul >
+                            <li ><a href="/services">Cierres</a></li>
+                            <!-- <li><a href="#">Búsqueda y seguro de títulos</a></li> -->
+                            <li><a href="/services">Servicio de depósito de garantía</a></li>
+                            <li><a href="/services">Contratos y documentos jurídicos</a></li>
+                            <li><a href="/services">Creación de Empresas</a></li>
+                            <li><a href="/services">Traducciones oficiales al español</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col  col-lg-4 col-md-6 col-sm-12 col-12">
+                    <div class="widget wpo-service-link-widget">
+                        <div class="widget-title">
+                            <h3>Contacto</h3>
+                        </div>
+                        <div class="contact-ft">
+                            <ul>
+                                <li><i class="fi flaticon-maps-and-flags"></i>C. Valerio González Plaza Tamarindo #4
+                                    <br> San José del Cabo, B.C.S. 23406</li>
+                                <li><i class="fi flaticon-phone-call"></i> <a style="color: #fff;"
+                                        href="tel:+526246901960">+52 624 690 1960</a> <br></li>
+                                <li><i class="fi flaticon-email"></i><a style="color: #fff;"
+                                        href="mailto:hola@phoenixservices.com.mx">hola@phoenixservices.com.mx</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div> <!-- end container -->
+    </div>
+    <div class="wpo-lower-footer">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col col-xs-12" style="color: #fff;">
+                    &copy; 2022 Phoenix Legal Services. Diseñado por
+                    <a style="color: #AC8D68;" href="https://www.bananagroup.mx" target="_blank">
+                        <strong>Banana Group Marketing</strong>
+                    </a>.
+                    <!-- <li><a href="#">Terms of use |</a> <a href="#">Privacy Environmental Policy</a></li> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end of wpo-site-footer-section -->
+
+<style>
+#whatsapp {
+    position: fixed;
+    z-index: 999;
+    bottom: 5rem;
+    right: 1rem;
+    width: 3rem;
+}
+</style>
+<a href="https://wa.me/+526241585988?text=Hola,%20me%20gustaría%20más%20información." target="_blank">
+    <img src="/assets/images/whats.png" alt="WhatsApp" id="whatsapp" />
+</a>
