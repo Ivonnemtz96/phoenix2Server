1343f6be13ad07c6a4875dee14a25a44f7e04d07
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-09 15:34:15 -0700

    ok, quité código hidden

--- en/services.php
+++ en/services.php
@@ -24,15 +24,16 @@
                 <div class="wpo-section-title">
                     <!-- <span>SUCCESSFUL PROJECT</span> -->
                     <h2 style="margin-bottom: 1rem;text-transform: none; text-align: center;">
-                        Buying a property in Cabo, we make it easy.
+                        Buying property in Cabo, we make it easy.
                     </h2>
                     <p style="color: #292929;">
 
-                        Our team of local experts is ready to guide you through the process of buying a property. We are
+                        Our team of local experts is ready to guide you through the process of buying property. We are
                         committed to fast, professional and friendly service to help you understand and feel comfortable
                         throughout the entire property buying process. Our attorneys specialize in the Baja California
-                        Sur area real estate market and are prepared to provide you with the right support <strong>from
-                            the beginning until you are officially the new owner!</strong>
+                        Sur area real estate market and are prepared to provide you with the right support <strong> from
+                            the
+                            beginning until you are officially the new owner!</strong>
 
                     </p>
                     <!-- <div class="shape-title">
@@ -59,7 +60,7 @@
                         <p>
                             We coordinate the real estate closing process with the real estate agents, the seller, the
                             buyer, the lender (if applicable), the trust bank, the appraiser, the notary, the government
-                            offices and everyone involved in the process. <br>
+                            offices and everyone involved in the process.
                             We promptly answer all your questions from the beginning and throughout the process, send
                             you reports, protect your interests and make sure the closing paperwork is correct, fair and
                             in legal order. We set up your Trust and complete all pre-closing tasks on your behalf, and
@@ -85,25 +86,25 @@
                 <li>
                     <div class="linea-txt">
                         <time>
-                            <h4 style="color: #fff;">Escrow services</h4>
+                            <h4 style="color: #fff;">ESCROW SERVICES</h4>
                         </time>
                         <p>
-                            We do not provide custodial services directly to our customers, but rather we use use
-                            established U.S. escrow companies, such as Global Escrow Solutions, Global Escrow Solutions
-                            Solutions, TLA and Armour Secure. These companies specialize in escrow services, offering
-                            the best level level of security required for all our clients.
+                            We do not provide escrow services directly to our customers, but rather we use established
+                            U.S. escrow companies, such as Global Escrow Solutions, Global Escrow Solutions Solutions,
+                            TLA and Armour Secure. These companies specialize in escrow services, offering the best
+                            level of security required for all our clients.
                         </p>
                     </div>
                 </li>
                 <li>
                     <div class="linea-txt">
                         <time>
-                            <h4 style="color: #fff;">Contracts and legal documents</h4>
+                            <h4 style="color: #fff;">CONTRACTS AND LEGAL DOCUMENTS</h4>
                         </time>
                         <p>
                             At Phoenix Legal Services our lawyers are able to assist you in all types of contracts that
                             our clients may need, such as: promise to trust, purchase and sale, construction, lease,
-                            joint ventures, etc. <br>
+                            joint ventures, etc.
                             We also draft other legal documents such as promissory notes, powers of attorney, attorney
                             reviews, due diligence reports, corporate resolutions, notices, official communications,
                             etc.
@@ -113,7 +114,7 @@
                 <li>
                     <div class="linea-txt">
                         <time>
-                            <h4 style="color: #fff;">Business Start-Ups</h4>
+                            <h4 style="color: #fff;">BUSINESS START-UPS</h4>
                         </time>
                         <p>
                             We can help you incorporate a Mexican company or corporation in less than two weeks. We
@@ -125,13 +126,12 @@
                 <li>
                     <div class="linea-txt">
                         <time>
-                            <h4 style="color: #fff;">Official Spanish translations</h4>
+                            <h4 style="color: #fff;">OFFICIAL SPANISH TRANSLATIONS</h4>
                         </time>
                         <p>
-                            All closing documents drafted in a foreign language must be translated into Spanish.
-                            Spanish, e.g., powers of attorney granted abroad, corporate documents of companies companies
-                            incorporated in other countries, etc. We have an official in-house translator for your
-                            convenience. convenience.
+                            All closing documents drafted in a foreign language must be translated into Spanish. e.g.,
+                            powers of attorney granted abroad, corporate documents of companies incorporated in other
+                            countries, etc. We have an official in-house translator for your convenience.
                         </p>
                     </div>
                 </li>
@@ -141,44 +141,43 @@
         <div class="container d-block d-md-none">
             <div class="row g-4 m-2">
                 <div class="col-12 p-4" style="background-color: #00000073;">
-                    <h4 style="color: #fff;">Closing</h4>
+                    <h4 style="color: #fff;">Closings</h4>
                     <p>
-                        We coordinate the real estate closing process with the real estate agents, seller, buyer, lender
-                        (if applicable), trustee bank, appraiser, notary, government offices and everyone else involved
-                        in the process. <br>
-
-                        We promptly answer all your questions from the beginning and throughout the process, send you
-                        reports, protect your interests and make sure the closing paperwork is correct, fair and in
-                        legal order. We set up your Trust and complete all pre-closing tasks on your behalf, and we
-                        stand by your side or represent you at closing. If the transaction involves financing (whether
-                        provided by the seller or a third party lender) we implement the necessary guarantees and
-                        instruments to secure the buyer's payment obligations.
+                        We coordinate the real estate closing process with the real estate agents, the seller, the
+                        buyer, the lender (if applicable), the trust bank, the appraiser, the notary, the government
+                        offices and everyone involved in the process.
+                        We promptly answer all your questions from the beginning and throughout the process, send
+                        you reports, protect your interests and make sure the closing paperwork is correct, fair and
+                        in legal order. We set up your Trust and complete all pre-closing tasks on your behalf, and
+                        we stand by your side or represent you at closing. If the transaction involves financing
+                        (whether provided by the seller or a third party lender) we implement the necessary
+                        guarantees and instruments to secure the buyer's payment obligations.
                     </p>
                 </div>
                 <div class="col-12 p-4" style="background-color: #00000073;">
 
-                    <h4 style="color: #fff;">Escrow services</h4>
-
+                    <h4 style="color: #fff;">ESCROW SERVICES</h4>
+                    </time>
                     <p>
-                        We do not provide custodial services directly to our customers, but rather we use use
-                        established U.S. escrow companies, such as Global Escrow Solutions, Global Escrow Solutions
-                        Solutions, TLA and Armour Secure. These companies specialize in escrow services, offering
-                        the best level level of security required for all our clients.
+                        We do not provide escrow services directly to our customers, but rather we use established
+                        U.S. escrow companies, such as Global Escrow Solutions, Global Escrow Solutions Solutions,
+                        TLA and Armour Secure. These companies specialize in escrow services, offering the best
+                        level of security required for all our clients.
                     </p>
                 </div>
                 <div class="col-12 p-4" style="background-color: #00000073;">
-                    <h4 style="color: #fff;">Contracts and legal documents</h4>
+                    <h4 style="color: #fff;">CONTRACTS AND LEGAL DOCUMENTS</h4>
                     <p>
                         At Phoenix Legal Services our lawyers are able to assist you in all types of contracts that
                         our clients may need, such as: promise to trust, purchase and sale, construction, lease,
-                        joint ventures, etc. <br>
+                        joint ventures, etc.
                         We also draft other legal documents such as promissory notes, powers of attorney, attorney
                         reviews, due diligence reports, corporate resolutions, notices, official communications,
                         etc.
                     </p>
                 </div>
                 <div class="col-12 p-4" style="background-color: #00000073;">
-                    <h4 style="color: #fff;">Business Start-Ups</h4>
+                    <h4 style="color: #fff;">BUSINESS START-UPS</h4>
                     <p>
                         We can help you incorporate a Mexican company or corporation in less than two weeks. We
                         obtain the name permit, draft the by-laws and powers of attorney (if applicable), and we
@@ -186,142 +185,16 @@
                     </p>
                 </div>
                 <div class="col-12 p-4" style="background-color: #00000073;">
-                    <h4 style="color: #fff;">Official Spanish translations</h4>
+                    <h4 style="color: #fff;">OFFICIAL SPANISH TRANSLATIONS</h4>
                     <p>
-                        All closing documents drafted in a foreign language must be translated into Spanish.
-                        Spanish, e.g., powers of attorney granted abroad, corporate documents of companies companies
-                        incorporated in other countries, etc. We have an official in-house translator for your
-                        convenience. convenience.
+                        All closing documents drafted in a foreign language must be translated into Spanish. e.g.,
+                        powers of attorney granted abroad, corporate documents of companies incorporated in other
+                        countries, etc. We have an official in-house translator for your convenience.
                     </p>
                 </div>
             </div>
         </div>
     </section>
 
-    <div hidden class="project-wrapper">
-        <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <!-- Primer servicio -->
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img class="img-fluid" src="/assets/images/project/1.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <h2>Cierres</h2>
-                    <p style="color: #AC8D68;">
-                        Coordinamos el proceso de cierre de la operación inmobiliaria con los agentes inmobiliarios, el
-                        vendedor, el comprador, el prestamista (si procede), el banco fiduciario, el tasador, el
-                        notario, las oficinas gubernamentales y todas las personas que participan en el proceso. <br>
-                        Respondemos rápidamente a todas sus preguntas desde el principio y durante todo el proceso, le
-                        enviamos informes, protegemos sus intereses y nos aseguramos de que el papeleo de cierre sea
-                        correcto, justo y esté en orden legal. Establecemos su Fideicomiso y completamos todas las
-                        tareas previas al cierre en su nombre, y estamos a su lado o le representamos en el cierre. Si
-                        la transacción involucra financiamiento (ya sea otorgado por el vendedor o por un tercer
-                        prestamista) implementamos las garantías e instrumentos necesarios para asegurar las
-                        obligaciones de pago del comprador.
-                    </p>
-                </div>
-
-                <!-- Segundo Servicio -->
-                <div class="col-lg-6">
-                    <h2>Búsqueda y seguro de títulos de propiedad</h2>
-                    <p style="color: #AC8D68;">
-                        Si usted desea obtener una póliza de seguro de título, ciertamente podemos integrarla al proceso
-                        de cierre. Nos encargamos de la búsqueda del título y de los documentos de apoyo, escribimos el
-                        informe del título y lo presentamos a la compañía de seguros de título. <br>
-                        La búsqueda del título, si se hace correctamente, le proporcionará una cadena del título de la
-                        propiedad, mostrará cualquiera restricción sobre la misma, e identificará cualquier derecho de
-                        retención relevante sobre la propiedad. La búsqueda de un título se lleva a cabo en los
-                        registros públicos de la propiedad locales de la ciudad, pueblo o condado donde se conservan
-                        tales registros de la propiedad.
-                    </p>
-                </div>
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img src="/assets/images/project/2.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Tercer Servicio -->
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img src="/assets/images/project/3.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <h2>Servicios de depósito en garantía</h2>
-                    <p style="color: #AC8D68;">
-                        No proporcionamos servicios de custodia directamente a nuestros clientes, sino que utilizamos
-                        empresas de custodia bien establecidas en Estados Unidos, tales como Global Escrow Solutions,
-                        TLA y Armour Secure. Estas empresas se especializan en los servicios de custodia ofreciendo el
-                        mejor
-                        nivel de seguridad requerido para todos nuestros clientes.
-                    </p>
-                </div>
-
-                <!-- cuarto servicio -->
-                <div class="col-lg-6">
-                    <h2>Contratos y documentos jurídicos</h2>
-                    <p style="color: #AC8D68;">
-                        En Phoenix Legal Services nuestros abogados están capacitados para asistirle en todo tipo de
-                        contratos que neustros clientes necesiten, como lo son: promesa a fideicomiso , compraventa,
-                        construcción,
-                        arrendamiento, joint ventures, etc. <br>
-                        También redactamos otros documentos legales como pagarés,
-                        poderes, revisiones de abogados, informes de due diligence, resoluciones corporativas,
-                        notificaciones, comunicaciones oficiales, etc.
-                    </p>
-                </div>
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img src="/assets/images/project/4.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Quinto servicio -->
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img src="/assets/images/project/5.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <h2>Empresas</h2>
-                    <p style="color: #AC8D68;">
-                        Podemos ayudarle a constituir una empresa o sociedad mexicana en menos de dos semanas. Obtenemos
-                        el permiso de nombre, redactamos los estatutos y los poderes (si son aplicables), y trabajamos
-                        con el notario mexicano para formalizar la escritura de constitución.
-                    </p>
-                </div>
-
-                <!-- Sexto Servicio -->
-                <div class="col-lg-6">
-                    <h2>Traducciones oficiales al español</h2>
-                    <p style="color: #AC8D68;">
-                        Todos los documentos de cierre redactados en un idioma extranjero deben ser traducidos al
-                        español, por ejemplo, poderes otorgados en el extranjero, documentos corporativos de empresas
-                        constituidas en otros países, etc. Disponemos de un traductor oficial interno para su comodidad.
-                    </p>
-                </div>
-                <div class="col-lg-6">
-                    <div class="project-single">
-                        <div class="project-single-img">
-                            <img src="/assets/images/project/6.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
 <!-- end wpo-project-section -->
