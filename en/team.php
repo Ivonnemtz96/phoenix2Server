f218a9602859e82c228ff5d142bd05d90f2b590d
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-09 15:24:12 -0700

    ok

--- en/team.php
+++ en/team.php
@@ -29,20 +29,17 @@
                     <div class="about-right">
                         <div class="text" style="padding: 15px 32px;">
                             <!-- <span>¿Quiénes somos?</span> -->
-                            <h2 style="font-size:25px;">Our closing company is 100% owned and managed by managed by
-                                attorneys.</h2>
+                            <h2 style="font-size:25px;">Our closing company is 100% owned and managed by attorneys.</h2>
                             <p>
                                 Phoenix Legal Services has been owned and operated by an attorney and a team of industry
                                 experts since its inception. Our efficient team has more than 30 years of combined
-                                experience in real estate services, which has now years of combined experience in real
-                                estate services, which has now made us the most reliable closing made closing services
-                                more reliable! <br><br>
+                                experience in real estate services, which has now made us the most reliable closing
+                                services! <br><br>
                                 As a relationship-driven real estate closing company, Phoenix Legal Services' strength
-                                lies in bringing people together to formulate our strength lies in bringing people
-                                together to formulate a successful transaction. successful transaction. Our attention to
-                                detail and exceptional customer service is what sets us apart. what sets us apart. We
-                                work to exceed expectations in every transaction, promising a smooth and seamless
-                                process for all parties.
+                                lies in bringing people together to formulate a successful transaction. Our attention to
+                                detail and exceptional customer service is what sets us apart. We work to exceed
+                                expectations in every transaction, promising a smooth and seamless process for all
+                                parties.
 
                             </p>
 
@@ -160,12 +157,10 @@
                 <a href="mailto:cesar@phoenixservices.com.mx "
                     style="color: #000;text-decoration: underline;">cesar@phoenixservices.com.mx </a> <br>
                 <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Cesar, attorney responsible for building long-term relationships with key Phoenix clients. Legal
-                    Services. For the past 20 years, Cesar has been involved in all aspects of the real estate industry
-                    (brokerage, development, construction, trusts and escrow). real estate industry (brokerage,
-                    development, construction, trusts and escrow), both domestically and internationally. escrow), both
-                    nationally and internationally. He brings a very high level of organization and to PLS's operation
-                    and aims to always be one step ahead of any potential problems so that they can be any potential
+                    Cesar, attorney responsible for building long-term relationships with key Phoenix clients. For the
+                    past 20 years, Cesar has been involved in all aspects of the real estate industry (brokerage,
+                    development, construction, trusts and escrow), both nationally and internationally. He brings a very
+                    high level of organization to PLS's operation and aims to always be one step ahead of any potential
                     problems so that they can be addressed quickly. Cesar lives in San Jose del Cabo, Baja California
                     Sur, with his wife and 2 children.
                 </p>
@@ -185,12 +180,12 @@
                 <a href="mailto:jordy@phoenixservices.com.mx"
                     style="color: #000;text-decoration: underline;">jordy@phoenixservices.com.mx</a> <br>
                 <p style="color: #414141; text-align: justify;margin-top:1rem;">
-                    Jordy attorney who focuses his practice on providing practical solutions to Phoenix clients. He is
+                    Jordy, attorney who focuses his practice on providing practical solutions to Phoenix clients. He is
                     attuned to the dynamic set of circumstances that bring the parties to a transaction together, so he
                     is able to advise his clients from the first step of the transaction through closing. Jordy's
                     co-workers and clients appreciate his depth of knowledge (he's a sweetheart!), with which you will
                     have a full and clear understanding of our services. <br>
-                    PS: He lives in shorts year round and keeps it relaxed!
+                    PS: He lives in shorts year-round and keeps it relaxed!
                 </p>
             </div>
         </div>
@@ -208,10 +203,9 @@
                     style="color: #000;text-decoration: underline;">karina@phoenixservices.com.mx</a> <br>
                 <p style="color: #414141; text-align: justify;margin-top:1rem;">
                     Karina began her professional career working for the federal government before moving into trusts.
-                    raising two amazing children, she learned to be super organized, flexible and approach life and work
-                    with straightforward honesty and a smart sense of humor. life and work with straightforward honesty
-                    and a smart sense of humor. When she's not in the office, Karina enjoys spending time with her
-                    children and friends.
+                    Raising two amazing children, she learned to be super organized, flexible and approach life and work
+                    with straightforward honesty and a smart sense of humor. When she's not in the office, Karina enjoys
+                    spending time with her children and friends.
 
                 </p>
             </div>
@@ -230,7 +224,10 @@
                     style="color: #000;text-decoration: underline;">eduardo@phoenixservices.com.mx</a> <br>
                 <p style="color: #414141; text-align: justify;margin-top:1rem;">
                     <a href=""></a>
-                    Eduardo has a degree in psychology which has served him well in becoming an indispensable member of the Phoenix Legal Services team. of the Phoenix Legal Services team. Although he does not act as an attorney, Eduardo keeps busy as an office administrator as office administrator and all the governmental procedures required by the trusts. trusts. When the Cabo heat permits, he tries not to miss the opportunity to walk his dogs or watch a sunset. his dogs or watch a sunset on the ocean.
+                    Eduardo has a degree in psychology which has served him well in becoming an indispensable member of
+                    the Phoenix Legal Services team. Although he does not act as an attorney, Eduardo is kept busy as an
+                    office administrator and with all the governmental procedures required by the trusts. When the Cabo
+                    heat permits, he tries not to miss the opportunity to walk his dogs or watch a sunset on the ocean.
                 </p>
             </div>
         </div>
